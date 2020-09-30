a4bf5b7b28364fd0d30fcbef180447098316eba7
phfernandes14 <contato.phfernandes@gmail.com>
2020-09-29 22:31:43 -0300

    - Criação do design da view de login
    - Criação do design da view de login

--- ProjetoEletivaCSharp/ProjetoEletivaCSharp/Form1.Designer.cs
+++ ProjetoEletivaCSharp/ProjetoEletivaCSharp/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProjetoEletivaCSharp
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,33 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.labelSenha = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.groupBoxLogin = new System.Windows.Forms.GroupBox();
+            this.groupBoxLogin.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLogin.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelLogin.Location = new System.Drawing.Point(55, 74);
+            this.labelLogin.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(61, 21);
+            this.labelLogin.TabIndex = 0;
+            this.labelLogin.Text = "Login: ";
+            // 
+            // labelSenha
+            // 
+            this.labelSenha.AutoSize = true;
+            this.labelSenha.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSenha.Location = new System.Drawing.Point(55, 123);
+            this.labelSenha.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelSenha.Name = "labelSenha";
+            this.labelSenha.Size = new System.Drawing.Size(61, 21);
+            this.labelSenha.TabIndex = 1;
+            this.labelSenha.Text = "Senha:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(144, 78);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 24);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(144, 127);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(209, 24);
+            this.textBox2.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(362, 174);
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(144, 182);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Sou foda";
+            this.button1.Size = new System.Drawing.Size(152, 49);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Entrar";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // groupBoxLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.groupBoxLogin.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.groupBoxLogin.Controls.Add(this.labelLogin);
+            this.groupBoxLogin.Controls.Add(this.button1);
+            this.groupBoxLogin.Controls.Add(this.textBox1);
+            this.groupBoxLogin.Controls.Add(this.textBox2);
+            this.groupBoxLogin.Controls.Add(this.labelSenha);
+            this.groupBoxLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBoxLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxLogin.Location = new System.Drawing.Point(194, 74);
+            this.groupBoxLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBoxLogin.Name = "groupBoxLogin";
+            this.groupBoxLogin.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBoxLogin.Size = new System.Drawing.Size(428, 282);
+            this.groupBoxLogin.TabIndex = 5;
+            this.groupBoxLogin.TabStop = false;
+            this.groupBoxLogin.Text = "Área de Login";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(797, 433);
+            this.Controls.Add(this.groupBoxLogin);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Login";
+            this.Text = "Login Usuário";
+            this.groupBoxLogin.ResumeLayout(false);
+            this.groupBoxLogin.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label labelSenha;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBoxLogin;
     }
 }
 
