7e1b8b853ababf7251449c8e914bee69ec60c0a6
igorgf25 <71769814+igorgf25@users.noreply.github.com>
2020-10-21 10:55:02 -0300

    - Criação Formulario Cadastro de Fornecedores

---
+++ ProjetoEletivaCSharp/ProjetoEletivaCSharp/FormCadastroCliente.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace ProjetoEletivaCSharp
+{
+    partial class FormCadastroCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelCNPJ = new System.Windows.Forms.Label();
+            this.labelRazao = new System.Windows.Forms.Label();
+            this.labelInsEstadual = new System.Windows.Forms.Label();
+            this.labelInsMunicipal = new System.Windows.Forms.Label();
+            this.labelNatureza = new System.Windows.Forms.Label();
+            this.labelEndereço = new System.Windows.Forms.Label();
+            this.labelTelefone = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.labelRamo = new System.Windows.Forms.Label();
+            this.txtCNPJ = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.txtRamo = new System.Windows.Forms.TextBox();
+            this.txtNatureza = new System.Windows.Forms.TextBox();
+            this.txtInsMunicipal = new System.Windows.Forms.TextBox();
+            this.txtInsEstadual = new System.Windows.Forms.TextBox();
+            this.txtRazao = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.bttnCadastrar = new System.Windows.Forms.Button();
+            this.bttnLimpar = new System.Windows.Forms.Button();
+            this.groupBoxCadastroFornecedores = new System.Windows.Forms.GroupBox();
+            this.groupBoxCadastroFornecedores.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelCNPJ
+            // 
+            this.labelCNPJ.AutoSize = true;
+            this.labelCNPJ.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCNPJ.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelCNPJ.Location = new System.Drawing.Point(13, 79);
+            this.labelCNPJ.Name = "labelCNPJ";
+            this.labelCNPJ.Size = new System.Drawing.Size(43, 16);
+            this.labelCNPJ.TabIndex = 1;
+            this.labelCNPJ.Text = "CNPJ";
+            // 
+            // labelRazao
+            // 
+            this.labelRazao.AutoSize = true;
+            this.labelRazao.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRazao.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelRazao.Location = new System.Drawing.Point(13, 116);
+            this.labelRazao.Name = "labelRazao";
+            this.labelRazao.Size = new System.Drawing.Size(89, 16);
+            this.labelRazao.TabIndex = 2;
+            this.labelRazao.Text = "Razão Social";
+            // 
+            // labelInsEstadual
+            // 
+            this.labelInsEstadual.AutoSize = true;
+            this.labelInsEstadual.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInsEstadual.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelInsEstadual.Location = new System.Drawing.Point(13, 152);
+            this.labelInsEstadual.Name = "labelInsEstadual";
+            this.labelInsEstadual.Size = new System.Drawing.Size(118, 16);
+            this.labelInsEstadual.TabIndex = 3;
+            this.labelInsEstadual.Text = "Inscrição Estadual";
+            // 
+            // labelInsMunicipal
+            // 
+            this.labelInsMunicipal.AutoSize = true;
+            this.labelInsMunicipal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInsMunicipal.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelInsMunicipal.Location = new System.Drawing.Point(13, 188);
+            this.labelInsMunicipal.Name = "labelInsMunicipal";
+            this.labelInsMunicipal.Size = new System.Drawing.Size(122, 16);
+            this.labelInsMunicipal.TabIndex = 4;
+            this.labelInsMunicipal.Text = "Inscrição Municipal";
+            // 
+            // labelNatureza
+            // 
+            this.labelNatureza.AutoSize = true;
+            this.labelNatureza.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNatureza.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelNatureza.Location = new System.Drawing.Point(13, 222);
+            this.labelNatureza.Name = "labelNatureza";
+            this.labelNatureza.Size = new System.Drawing.Size(112, 16);
+            this.labelNatureza.TabIndex = 5;
+            this.labelNatureza.Text = "Natureza Juridica";
+            // 
+            // labelEndereço
+            // 
+            this.labelEndereço.AutoSize = true;
+            this.labelEndereço.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEndereço.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelEndereço.Location = new System.Drawing.Point(13, 286);
+            this.labelEndereço.Name = "labelEndereço";
+            this.labelEndereço.Size = new System.Drawing.Size(67, 16);
+            this.labelEndereço.TabIndex = 6;
+            this.labelEndereço.Text = "Endereço";
+            // 
+            // labelTelefone
+            // 
+            this.labelTelefone.AutoSize = true;
+            this.labelTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.labelTelefone.Location = new System.Drawing.Point(70, 609);
+            this.labelTelefone.Name = "labelTelefone";
+            this.labelTelefone.Size = new System.Drawing.Size(49, 13);
+            this.labelTelefone.TabIndex = 7;
+            this.labelTelefone.Text = "Telefone";
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.labelEmail.Location = new System.Drawing.Point(70, 641);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(32, 13);
+            this.labelEmail.TabIndex = 8;
+            this.labelEmail.Text = "Email";
+            // 
+            // labelRamo
+            // 
+            this.labelRamo.AutoSize = true;
+            this.labelRamo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRamo.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelRamo.Location = new System.Drawing.Point(13, 254);
+            this.labelRamo.Name = "labelRamo";
+            this.labelRamo.Size = new System.Drawing.Size(119, 16);
+            this.labelRamo.TabIndex = 9;
+            this.labelRamo.Text = "Ramo de Negocio";
+            // 
+            // txtCNPJ
+            // 
+            this.txtCNPJ.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCNPJ.Location = new System.Drawing.Point(139, 79);
+            this.txtCNPJ.Name = "txtCNPJ";
+            this.txtCNPJ.Size = new System.Drawing.Size(219, 22);
+            this.txtCNPJ.TabIndex = 10;
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEndereco.Location = new System.Drawing.Point(139, 286);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(219, 22);
+            this.txtEndereco.TabIndex = 11;
+            // 
+            // txtRamo
+            // 
+            this.txtRamo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRamo.Location = new System.Drawing.Point(139, 254);
+            this.txtRamo.Name = "txtRamo";
+            this.txtRamo.Size = new System.Drawing.Size(219, 22);
+            this.txtRamo.TabIndex = 12;
+            // 
+            // txtNatureza
+            // 
+            this.txtNatureza.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNatureza.Location = new System.Drawing.Point(139, 222);
+            this.txtNatureza.Name = "txtNatureza";
+            this.txtNatureza.Size = new System.Drawing.Size(219, 22);
+            this.txtNatureza.TabIndex = 13;
+            // 
+            // txtInsMunicipal
+            // 
+            this.txtInsMunicipal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInsMunicipal.Location = new System.Drawing.Point(139, 186);
+            this.txtInsMunicipal.Name = "txtInsMunicipal";
+            this.txtInsMunicipal.Size = new System.Drawing.Size(219, 22);
+            this.txtInsMunicipal.TabIndex = 14;
+            // 
+            // txtInsEstadual
+            // 
+            this.txtInsEstadual.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInsEstadual.Location = new System.Drawing.Point(139, 149);
+            this.txtInsEstadual.Name = "txtInsEstadual";
+            this.txtInsEstadual.Size = new System.Drawing.Size(219, 22);
+            this.txtInsEstadual.TabIndex = 15;
+            // 
+            // txtRazao
+            // 
+            this.txtRazao.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRazao.Location = new System.Drawing.Point(139, 113);
+            this.txtRazao.Name = "txtRazao";
+            this.txtRazao.Size = new System.Drawing.Size(219, 22);
+            this.txtRazao.TabIndex = 16;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(196, 606);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(219, 20);
+            this.txtTelefone.TabIndex = 17;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(196, 638);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(219, 20);
+            this.txtEmail.TabIndex = 18;
+            // 
+            // bttnCadastrar
+            // 
+            this.bttnCadastrar.Location = new System.Drawing.Point(423, 92);
+            this.bttnCadastrar.Name = "bttnCadastrar";
+            this.bttnCadastrar.Size = new System.Drawing.Size(125, 58);
+            this.bttnCadastrar.TabIndex = 19;
+            this.bttnCadastrar.Text = "Cadastrar";
+            this.bttnCadastrar.UseVisualStyleBackColor = true;
+            // 
+            // bttnLimpar
+            // 
+            this.bttnLimpar.Location = new System.Drawing.Point(423, 211);
+            this.bttnLimpar.Name = "bttnLimpar";
+            this.bttnLimpar.Size = new System.Drawing.Size(125, 56);
+            this.bttnLimpar.TabIndex = 20;
+            this.bttnLimpar.Text = "Limpar";
+            this.bttnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxCadastroFornecedores
+            // 
+            this.groupBoxCadastroFornecedores.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.groupBoxCadastroFornecedores.Controls.Add(this.bttnLimpar);
+            this.groupBoxCadastroFornecedores.Controls.Add(this.labelCNPJ);
+            this.groupBoxCadastroFornecedores.Controls.Add(this.bttnCadastrar);
+            this.groupBoxCadastroFornecedores.Controls.Add(this.labelRazao);
+            this.groupBoxCadastroFornecedores.Controls.Add(this.labelInsEstadual);
+            this.groupBoxCadastroFornecedores.Controls.Add(this.labelInsMunicipal);
+            this.groupBoxCadastroFornecedores.Controls.Add(this.txtRazao);
+            this.groupBoxCadastroFornecedores.Controls.Add(this.labelNatureza);
+            this.groupBoxCadastroFornecedores.Controls.Add(this.txtInsEstadual);
+            this.groupBoxCadastroFornecedores.Controls.Add(this.labelEndereço);
+            this.groupBoxCadastroFornecedores.Controls.Add(this.txtInsMunicipal);
+            this.groupBoxCadastroFornecedores.Controls.Add(this.labelRamo);
+            this.groupBoxCadastroFornecedores.Controls.Add(this.txtNatureza);
+            this.groupBoxCadastroFornecedores.Controls.Add(this.txtCNPJ);
+            this.groupBoxCadastroFornecedores.Controls.Add(this.txtRamo);
+            this.groupBoxCadastroFornecedores.Controls.Add(this.txtEndereco);
+            this.groupBoxCadastroFornecedores.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxCadastroFornecedores.Location = new System.Drawing.Point(88, 28);
+            this.groupBoxCadastroFornecedores.Name = "groupBoxCadastroFornecedores";
+            this.groupBoxCadastroFornecedores.Size = new System.Drawing.Size(616, 369);
+            this.groupBoxCadastroFornecedores.TabIndex = 21;
+            this.groupBoxCadastroFornecedores.TabStop = false;
+            this.groupBoxCadastroFornecedores.Text = "Cadastro de Fornecedores";
+            this.groupBoxCadastroFornecedores.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // FormCadastroCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(800, 443);
+            this.Controls.Add(this.groupBoxCadastroFornecedores);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.labelTelefone);
+            this.Name = "FormCadastroCliente";
+            this.Text = "FormCadastroCliente";
+            this.groupBoxCadastroFornecedores.ResumeLayout(false);
+            this.groupBoxCadastroFornecedores.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelCNPJ;
+        private System.Windows.Forms.Label labelRazao;
+        private System.Windows.Forms.Label labelInsEstadual;
+        private System.Windows.Forms.Label labelInsMunicipal;
+        private System.Windows.Forms.Label labelNatureza;
+        private System.Windows.Forms.Label labelEndereço;
+        private System.Windows.Forms.Label labelTelefone;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.Label labelRamo;
+        private System.Windows.Forms.TextBox txtCNPJ;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.TextBox txtRamo;
+        private System.Windows.Forms.TextBox txtNatureza;
+        private System.Windows.Forms.TextBox txtInsMunicipal;
+        private System.Windows.Forms.TextBox txtInsEstadual;
+        private System.Windows.Forms.TextBox txtRazao;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button bttnCadastrar;
+        private System.Windows.Forms.Button bttnLimpar;
+        private System.Windows.Forms.GroupBox groupBoxCadastroFornecedores;
+    }
+}
